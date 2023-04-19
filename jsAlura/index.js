7397ea06568b1f584c35fdead5d79d41421cf072
Renan <renan_acm1992@hotmail.com>
2023-04-19 16:43:00 -0300

    estudos alura dia 19/04/2023

--- jsAlura/index.js
+++ jsAlura/index.js
@@ -1,44 +1,22 @@
-class Cliente {
-  nome;
-  cpf;
-}
-
-class ContaCorrente {
-  agencia;
-  // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
-  _saldo = 0;
-
-  sacar(valor) {
-    if (this._saldo >= valor) {
-      this._saldo -= valor;
-      return valor;
-    }
-  }
-
-  depositar(valor) {
-    if (valor <= 0) {
-      return;
-    }
-    this._saldo += valor;
-  }
-}
+import { Cliente } from "./Cliente.js";
+import { ContaCorrente } from "./ContaCorrente.js";
 
 const cliente1 = new Cliente();
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
 
-const cliente2 = new Cliente();
-cliente2.nome = "Alice";
-cliente2.cpf = 88822233309;
-
 const contaCorrenteRicardo = new ContaCorrente();
 contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.cliente = cliente1
+contaCorrenteRicardo.depositar(500)
 
-contaCorrenteRicardo.depositar(-100);
-contaCorrenteRicardo.depositar(100);
-contaCorrenteRicardo.depositar(100);
 
-const valorSacado = contaCorrenteRicardo.sacar(50);
-console.log(valorSacado);
+const conta2 = new ContaCorrente();
+conta2.cliente = new Cliente();
+conta2.cliente.nome = "Alice";
+conta2.cliente.cpf = 88822233309;
+conta2.agencia = 102;
 
-console.log(contaCorrenteRicardo);
+let valor = 200
+contaCorrenteRicardo.transferir(valor, conta2)
+
