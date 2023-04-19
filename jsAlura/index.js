adfc3fdf415865ac7d100ee50ba2531a40412dce
renan-augusto <renan_acm1992@hotmail.com>
2023-04-19 07:05:27 -0300

    arquivo da aula 2 alura: JavaScript: programando a Orientacao a Objetos

--- jsAlura/index.js
+++ jsAlura/index.js
@@ -5,12 +5,21 @@ class Cliente {
 
 class ContaCorrente {
   agencia;
-  saldo;
+  // #saldo =0 https://github.com/tc39/proposal-class-fields#private-fields
+  _saldo = 0;
 
   sacar(valor) {
-    if (this.saldo >= valor) {
-      this.saldo -= valor;
+    if (this._saldo >= valor) {
+      this._saldo -= valor;
+      return valor;
     }
+  }
+
+  depositar(valor) {
+    if (valor <= 0) {
+      return;
+    }
+    this._saldo += valor;
   }
 }
 
@@ -20,12 +29,16 @@ cliente1.cpf = 11122233309;
 
 const cliente2 = new Cliente();
 cliente2.nome = "Alice";
-cliente2.cpf = 33322211109;
+cliente2.cpf = 88822233309;
 
 const contaCorrenteRicardo = new ContaCorrente();
-contaCorrenteRicardo.saldo = 100;
 contaCorrenteRicardo.agencia = 1001;
-contaCorrenteRicardo.sacar(50);
 
-console.log(cliente1);
-console.log(cliente2);
+contaCorrenteRicardo.depositar(-100);
+contaCorrenteRicardo.depositar(100);
+contaCorrenteRicardo.depositar(100);
+
+const valorSacado = contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+console.log(contaCorrenteRicardo);
