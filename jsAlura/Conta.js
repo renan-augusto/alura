91537a003fd4e5c322fa4a048e151a45dfabe659
Renan <renan_acm1992@hotmail.com>
2023-04-20 17:02:31 -0300

    Estudos tarde

--- jsAlura/Conta.js
+++ jsAlura/Conta.js
@@ -1,15 +1,42 @@
 export class Conta {
   constructor(saldoInicial, cliente, agencia) {
+    if(this.constructor == Conta) {
+      throw new Error ("Não instanciar um objeto do tipo conta")
+    }
     this._saldo = saldoInicial;
     this._cliente = cliente;
     this._agencia = agencia;
+
   }
 
-  sacar(valor) {
-    if (this._saldo >= valor) {
-      this._saldo -= valor;
-      return valor;
+  set cliente(novoValor) {
+    if (novoValor instanceof Cliente) {
+      this._cliente = novoValor;
     }
+  }
+
+  get cliente() {
+    return this._cliente;
+  }
+
+  get saldo() {
+    return this._saldo;
+  }
+
+
+  sacar(valor) {
+    let taxa = 1
+    return this._sacar(valor, taxa);
+  }
+
+  _sacar(valor, taxa){
+    const valorSacado = taxa * valor;
+    if(this._saldo >= valorSacado) {
+      this._saldo -= valorSacado;
+      return valorSacado;
+    }
+
+    return 0;
   }
 
   depositar(valor) {
