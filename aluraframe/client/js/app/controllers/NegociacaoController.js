e8c6758d4bc73092d38e41fde51e93e3eb074681
renan-augusto <renan_acm1992@hotmail.com>
2023-05-02 07:18:10 -0300

    estudos alura 02/05 manha

--- aluraframe/client/js/app/controllers/NegociacaoController.js
+++ aluraframe/client/js/app/controllers/NegociacaoController.js
@@ -4,11 +4,31 @@ class NegociacaoController {
     this._inputData = $("#data");
     this._inputQuantidade = $("#quantidade");
     this._inputValor = $("#valor");
-    this._listaNegociacoes = new ListaNegociacoes();
+
+    /*
+    this._listaNegociacoes = new ListaNegociacoes((model) =>
+      this._negociacoesView.update(model)
+    ); */
+
+    let self = this;
+    this._listaNegociacoes = new Proxy(new ListaNegociacoes(), {
+      get(target, prop, receiver) {
+        if (
+          ["adiciona", "esvazia"].includes(prop) &&
+          typeof target[prop] == typeof Function
+        ) {
+          return function () {
+            console.log(`interceptando ${prop}`);
+            Reflect.apply(target[prop], target, arguments);
+            self._negociacoesView.update(target);
+          };
+        }
+        return Reflect.get(target, prop, receiver);
+      },
+    });
 
     this._negociacoesView = new NegociacoesView($("#negociacoesView"));
     this._negociacoesView.update(this._listaNegociacoes);
-
     this._mensagem = new Mensagem();
     this._mensagemView = new MensagemView($("#mensagemView"));
     this._mensagemView.update(this._mensagem);
@@ -17,7 +37,6 @@ class NegociacaoController {
   adiciona(event) {
     event.preventDefault();
     this._listaNegociacoes.adiciona(this._criaNegociacao());
-    this._negociacoesView.update(this._listaNegociacoes);
 
     this._mensagem.texto = "Negociação adicionada com sucesso";
     this._mensagemView.update(this._mensagem);
@@ -27,7 +46,6 @@ class NegociacaoController {
 
   apaga() {
     this._listaNegociacoes.esvazia();
-    this._negociacoesView(this._listaNegociacoes);
 
     this._mensagem.texto = "Negociações apagadas com sucesso";
     this._mensagemView.update(this._mensagem);
