e70efa63ce513433c8038bbae95c51705a394180
renan-augusto <renan_acm1992@hotmail.com>
2023-05-19 07:16:42 -0300

    alura manha20230519

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -2,5 +2,8 @@ import { handleStatus, log } from "./utils/promise-helpers.js";
 import "./utils/array-helpers.js";
 import { notasService as service } from "./nota/service.js";
 
+const ehDivisivel = (divisor, numero) => !(numero % divisor);
+const ehDivisivelPorDois = ehDivisivel.bind(null, 2);
+
 document.querySelector("#myButton").onclick = () =>
   service.sumItems("2143").then(console.log).catch(console.log);
--- project/public/app/nota/service.js
+++ project/public/app/nota/service.js
@@ -1,19 +1,29 @@
 import { handleStatus } from "../utils/promise-helpers";
+import { partialize } from "../utils/operators";
 
 const API = "https://localhost:3000/notas";
 
-const sumItems = (code) => (notas) =>
-  notas
-    .$flatMap((nota) => nota.itens)
-    .filter((item) => item.codigo == code)
-    .reduce((total, item) => total + item.valor, 0);
+const getItemsFromNotas = (notas) => notas.$flatMap((nota) => nota.itens);
+const filterItemsByCode = (code, items) =>
+  items.filter((item) => item.codigo == code);
+const sumItemsValue = (items) =>
+  items.reduce((total, item) => total + item.valor, 0);
 
 export const notasService = {
   listAll() {
-    return fetch(API).then(handleStatus);
+    return fetch(API)
+      .then(handleStatus)
+      .catch((err) => {
+        console.log(err);
+        return Promise.reject("Nao foi possivel obter as notas fiscais");
+      });
   },
 
   sumItems(code) {
-    return this.listAll().then(sumItems(code));
+    const filterItems = partialize(filterItemsByCode, code);
+
+    return this.listAll().then((notas) =>
+      sumItemsValue(filterItems(getItemsFromNotas(notas)))
+    );
   },
 };
