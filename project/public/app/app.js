f54d3211dd1ecaaac3cc8d9d2c3f840b41fdf83a
renan-augusto <renan_acm1992@hotmail.com>
2023-05-22 06:56:59 -0300

    estudos alura 22052023 manha

--- project/public/app/app.js
+++ project/public/app/app.js
@@ -1,15 +1,16 @@
-import { handleStatus, log } from "./utils/promise-helpers.js";
+import { handleStatus, log, timeoutPromise } from "./utils/promise-helpers.js";
 import "./utils/array-helpers.js";
 import { notasService as service } from "./nota/service.js";
 import { takeUntil, debounceTime, partialize } from "./utils/operators.js";
 
+Promise.race([p1, p2]).then(console.log).catch(console.log);
+
 const operation = pipe(partialize(debounceTime, 500), partialize(takeUntil, 3));
 
-const action = debounceTime(
-  500,
-  takeUntil(3, () =>
-    service.sumItems("2143").then(console.log).catch(console.log)
-  )
+const action = operation(() =>
+  timeoutPromise(200, service.sumItems("2143"))
+    .then(console.log)
+    .catch(console.log)
 );
 
 document.querySelector("#myButton").onclick = () => operation();
--- project/public/app/utils/promise-helpers.js
+++ project/public/app/utils/promise-helpers.js
@@ -5,3 +5,13 @@ export const log = (param) => {
   console.log(param);
   return param;
 };
+
+export const timeoutPromise = (milliseconds, promise) => {
+  const timeout = new Promise((relve, reject) =>
+    setTimeout(
+      () => reject(`Limite da promise excedido (limite ${milliseconds} ms)`),
+      milliseconds
+    )
+  );
+  return Promise.race([timeout, promise]);
+};
